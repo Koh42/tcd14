74e532e1297e7074048615a84cb0fe6f6212f4b9
SM.K <sweemeng.koh@gmail.com>
2023-12-18 13:10:20 +0800

    WIP: add nginx features: modsecurity, static, force-https, gzip

--- images/nginx/entrypoint.sh
+++ images/nginx/entrypoint.sh
@@ -5,7 +5,7 @@ if [ $LEN -eq 0 ] || [ $LEN -gt 64 ]; then
 	SSL_DOMAIN=Localhost
 fi
 
-SSL=/etc/nginx/http.d/ssl
+SSL=/etc/nginx/conf.d/ssl
 if [ ! -e $SSL.key ] || [ ! -e $SSL.crt ]; then
 	openssl req -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 \
 		-subj "/CN=${SSL_DOMAIN}/O=${SSL_DOMAIN}/C=MY" -keyout $SSL.key -out $SSL.crt
